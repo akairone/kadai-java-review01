95ee89ba2789fd5d07372d99cd0b9138fec3d6f2
akairone <okame629@icloud.com>
2024-05-17 00:05:08 +0900

    Javaプログラミング入門学習完了

---
+++ src/ArithmeticExcentionSample.java
@@ -0,0 +1,13 @@
+
+public class ArithmeticExcentionSample {
+
+	public static void main(String[] args) {
+		// int型変数10をint型変数0で除算
+		int a = 10 / 0; // ここで例外が発生！
+
+		// この下の出力は実行されない！
+		System.out.println(a);
+
+	}
+
+}
