76a5a95a9242c3a1affd336665d023f696a52761
akairone <okame629@icloud.com>
2024-05-18 18:41:22 +0900

    課題再提出用にメソッド名とキャストの使用をしました。

--- src/Review01.java
+++ src/Review01.java
@@ -2,17 +2,17 @@
 public class Review01 {
 
 	public static void main(String[] args) {
-		double price = 1500;
-		double taxRate = 10;
+		int price = 1500;
+		int taxRate = 10;
 
-		double tax = taxMethod(price, taxRate);
-		System.out.println(Math.round(price) + "円の商品の税込価格は" +Math.round(price + tax) + "円（消費税は" + Math.round(tax) + "円）です。");
+		int tax = (int) tax(price, taxRate);
+		System.out.println ( price + "円の商品の税込価格は" + (price + tax) + "円（消費税は" + tax + "円）です。");
 
 	}
 
-	public static double taxMethod(double price,double taxRate) {
+	public static double tax(double price,double taxRate) {
 
-		double tax = (price * (taxRate/100));
+		double tax = price * (taxRate/100);
 		return tax;
 	}
 }
